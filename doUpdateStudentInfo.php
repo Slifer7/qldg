080423983621b56293e609efffddb3501394a41e
tdquang7@gmail.com <tdquang7@gmail.com>
2016-05-05 23:37:39 +0700

    backup

---
+++ doUpdateStudentInfo.php
@@ -0,0 +1,9 @@
+<?php
+if (isset($_POST["newID"], $_POST["newFullName"]))
+{
+  $newID = $_POST["newID"];
+  $newFullName = $_POST["newFullName"];
+  
+  echo "Success";
+}
+?>
--- js/registration.js
+++ js/registration.js
@@ -1,33 +1,50 @@
 var gClipboard;
 var gIsEditing = false;
+var gID;
+var gFullName;
 
 function beginEdit(id){
 	if (gIsEditing == false){
 		gIsEditing = true;
-		
+
 		var tr = $("#" + id);
-		gClipboard = tr.html();	// Lưu dữ liệu ban đầu để phục hồi		
+		gClipboard = tr.html();	// Lưu dữ liệu ban đầu để phục hồi
 		var childs = tr.children("td");
+		gID = childs[0];
+		gFullName = childs[1];
 
 		// Chỉ cho phép thay đổi MSSV và Họ tên thôi vì mấy cái còn lại loại suy ra được.
-		childs[0].innerHTML = "<input id='' style='width: 70px;' type='text' value='{0}'/>".format(childs[0].innerText); 
-		childs[1].innerHTML = "<input style='width: 180px;' type='text' value='{0}'/>".format(childs[1].innerText); 
-		 
+		gID.innerHTML = "<input id='newID' style='width: 70px;' type='text' value='{0}'/>".format(childs[0].innerText);
+		gFullName.innerHTML = "<input id='newFullName' style='width: 180px;' type='text' value='{0}'/>".format(childs[1].innerText);
+
 		// Đổi nút Edit thành hai nút khác, OK và Cancel
-		childs[4].innerHTML = "<a href='endEdit' onclick='endEdit'>OK</a> &nbsp;<a href='cancelEdit' onclick='return cancelEdit({0});'>Hủy</a>".format(id);
+		childs[4].innerHTML = "<a href='endEdit' onclick='return endEdit({0});'>OK</a> &nbsp;<a href='cancelEdit1' onclick='return cancelEdit({1});'>Hủy</a>".format(id, id);
 	}
 	else{
 		alert('Bạn đang hiệu chỉnh dữ liệu ở dòng khác.');
 	}
-	
+
 	return false; // Đi từ thẻ a nên cần return false để sự kiện click link không xảy ra
 }
 
-function endEdit(id){	
+function endEdit(id){
 	var tr = $("#" + id);
-	var childs = tr.children("td");
-	
-	
+	var newID = $("#newID").val();
+	var newFullName = $("#newFullName").val();
+
+	// Lấy dữ liệu mới
+	$.ajax({"url": "doUpdateStudentInfo.php",
+		"type" : "POST",
+		"data" : {
+			"newID" : newID,
+			"newFullName"   : newFullName
+		},
+		"success" : function(data){
+				console.log(data);
+				alert("Đã cập nhật thành công cho sinh viên có mã số: " + newID);
+		}
+	});
+
 	return false; // Đi từ thẻ a nên cần return false để sự kiện click link không xảy ra
 }
 
@@ -35,6 +52,6 @@ function cancelEdit(id){
 	//Phục hồi dữ liệu hiện tại từ clipboard
 	$("#" + id).html(gClipboard);
 	gIsEditing = false;
-	
+
 	return false; // Đi từ thẻ a nên cần return false để sự kiện click link không xảy ra
 }
